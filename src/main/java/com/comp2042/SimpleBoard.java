101a227da60faea296f988e1a6b261d1ce55047c
whntc75 <2815949448@qq.com>
2025-11-20 22:32:04 +0800

    Fix the error in the spawn position height of the blocks
    I found that the initial spawn position of the blocks is in the middle of the game interface, and the "Game Over" message appears when the block height reaches the middle position (i.e., the initial spawn position) rather than when it reaches the top of the game interface. Therefore, I modified the Y-axis value to make the blocks spawn from the correct initial position—the top of the game interface—and ensured that "Game Over" only shows up when the block height reaches the top of the game interface.

--- src/main/java/com/comp2042/SimpleBoard.java
+++ src/main/java/com/comp2042/SimpleBoard.java
@@ -85,7 +85,7 @@ public class SimpleBoard implements Board {
     public boolean createNewBrick() {
         Brick currentBrick = brickGenerator.getBrick();
         brickRotator.setBrick(currentBrick);
-        currentOffset = new Point(4, 10);
+        currentOffset = new Point(4, 0);
         return MatrixOperations.intersect(currentGameMatrix, brickRotator.getCurrentShape(), (int) currentOffset.getX(), (int) currentOffset.getY());
     }
 
