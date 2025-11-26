9b56ec1dddc34d447a34dccb17f4e5527ea7e8db
whntc75 <2815949448@qq.com>
2025-11-26 23:15:13 +0800

    Introduce lines property to Score class
    Added lines property to track total lines cleared.

--- src/main/java/com/comp2042/Score.java
+++ src/main/java/com/comp2042/Score.java
@@ -6,16 +6,25 @@ import javafx.beans.property.SimpleIntegerProperty;
 public final class Score {
 
     private final IntegerProperty score = new SimpleIntegerProperty(0);
+    private final IntegerProperty lines = new SimpleIntegerProperty(0);//record the total number of lines cleared
 
     public IntegerProperty scoreProperty() {
         return score;
+    }
+
+      public IntegerProperty linesProperty() {
+        return lines;
     }
 
     public void add(int i){
         score.setValue(score.getValue() + i);
     }
 
+    public void addLines(int n) {lines.setValue(lines.getValue() + n);}//Accumulate the number of lines cleared
+
+
     public void reset() {
         score.setValue(0);
+        lines.setValue(0);//Reset the line count to zero when starting a new game
     }
 }
