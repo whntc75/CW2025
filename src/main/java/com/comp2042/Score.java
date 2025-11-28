f9c3942e08138694825b40c7159e97e1a5e2d226
whntc75 <2815949448@qq.com>
2025-11-28 22:41:59 +0800

    Add level tracking and update line clearing logic
    Added a level property and updated the line clearing logic so the level increases every 10 lines cleared.

--- src/main/java/com/comp2042/Score.java
+++ src/main/java/com/comp2042/Score.java
@@ -7,6 +7,7 @@ public final class Score {
 
     private final IntegerProperty score = new SimpleIntegerProperty(0);
     private final IntegerProperty lines = new SimpleIntegerProperty(0);//record the total number of lines cleared
+    private final IntegerProperty level = new SimpleIntegerProperty(1);//The level starts at 1
 
     public IntegerProperty scoreProperty() {
         return score;
@@ -16,15 +17,22 @@ public final class Score {
         return lines;
     }
 
+     public IntegerProperty levelProperty() { return level; }//Return the int value of the current level
+
     public void add(int i){
         score.setValue(score.getValue() + i);
     }
 
-    public void addLines(int n) {lines.setValue(lines.getValue() + n);}//Accumulate the number of lines cleared
+    public void addLines(int n) {
+    lines.setValue(lines.getValue() + n);//Accumulate the number of lines cleared
+    int totalLines = lines.get();      //The total number of lines cleared
+    int newLevel = totalLines / 10 + 1; //Level up every time 10 lines are cleared
+    level.set(newLevel);}
 
 
     public void reset() {
         score.setValue(0);
         lines.setValue(0);//Reset the line count to zero when starting a new game
+        level.setValue(1);
     }
 }
